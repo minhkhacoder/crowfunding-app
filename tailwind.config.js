66d6936d10b7d31ce44b528dab5def2b6a8d03cc
minhkhacoder <minhkhawork@gmail.com>
2022-09-26 22:09:28 +0700

    Set up styling and structure folder

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @format */
+
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ["Epilogue", "sans-serif;"],
+      },
+      colors: {
+        primary: "#1DC071",
+        secondary: "#6F49FD",
+        text1: "#3A3A43",
+        text2: "#4B5264",
+        text3: "#808191",
+        text4: "#B2B3BD",
+        "icon-color": "#A2A2A8",
+        white: "#ffffff",
+        "white-soft": "#FCFBFF",
+        "gray-soft": "#FCFCFC",
+        strock: "#F1F1F3",
+        "lite-bg": "#FCFCFD",
+        error: "#EB5757",
+        "dark-bg": "#3A3A43",
+        "dark-secondary": "#1C1C24",
+        "dark-soft": "#22222C",
+        "soft-dark": "#24242C",
+        "dark-stroke": "#3A3A43",
+        "dark-red": "#422C32",
+      },
+    },
+  },
+  plugins: [],
+};
